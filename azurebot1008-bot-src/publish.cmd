26cd70bc26d8ff859d132f28095147f1461a059b
Nalam Manasa Venkata Naga Sai <93673311+NalamManasa@users.noreply.github.com>
2022-09-28 17:18:48 +0530

    Add files via upload

---
+++ azurebot1008-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\azurebot1008-bot-a4e6-Web-Deploy.pubxml -p:Password=g0SvvBopNTPMceApcfB8oojbMQjaKfcoehuJvFRRuQR4YKFfZc659i4TgJho
+
